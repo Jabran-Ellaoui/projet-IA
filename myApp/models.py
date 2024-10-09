f4e2eb798aea534dfd4aed8f7da32d65d43b24fb
Jojolevrai <josue.lefour@gmail.com>
2024-10-09 09:13:18 +0200

    db v3

--- myApp/models.py
+++ myApp/models.py
@@ -10,6 +10,8 @@ class Player(db.Model):
     id_player = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     is_human = db.Column(db.Boolean, nullable=False)  # True for human, False for AI
+    playerpos_x = db.Column(db.Integer, nullable=False)
+    playerpos_y = db.Column(db.Integer, nullable=False)
 
     def __init__(self, name, is_human):
         self.name = name
@@ -22,6 +24,8 @@ class Game(db.Model):
     player1_id = db.Column(db.Integer, db.ForeignKey('players.id_player'), nullable=False)
     player2_id = db.Column(db.Integer, db.ForeignKey('players.id_player'), nullable=False)
     winner = db.Column(db.String(100), nullable=True)  # Peut être NULL si pas encore de gagnant
+    boxes1 = db.Column(db.String(100), nullable=True) # une lettre représente une case, 1 pour joueur 1, 2 joueur 2 ,
+    # 0 aucun joueur. 25 lettres, lignes espacées par un espace ducoup xxxxx xxxxx xxxxx xxxxx xxxxx
 
     def __init__(self, player1_id, player2_id, winner):
         self.player1_id = player1_id
@@ -33,9 +37,9 @@ class Move(db.Model):
 
     id_move = db.Column(db.Integer, primary_key=True, autoincrement=True)
     game_id = db.Column(db.Integer, db.ForeignKey('games.id_game'), nullable=False)
-    player = db.Column(db.String(100), nullable=False)  # Peut être "player1" ou "player2"
-    x_coordinate = db.Column(db.Integer, nullable=False)  # Coordonnée x (0-4 pour une grille 5x5)
-    y_coordinate = db.Column(db.Integer, nullable=False)  # Coordonnée y (0-4 pour une grille 5x5)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id_player'))
+    x_coordinate = db.Column(db.Integer, nullable=False)  # Coordonnée x
+    y_coordinate = db.Column(db.Integer, nullable=False)  # Coordonnée y
 
     def __init__(self, game_id, player, x_coordinate, y_coordinate):
         self.game_id = game_id
