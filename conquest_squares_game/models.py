bdcf58416a4c0730bdbf20693859132ccf950bfc
GyuriMero <128895830+GyuriMero@users.noreply.github.com>
2024-12-04 15:19:19 +0100

    changed to correspond to the MAIN
    j'ai remplacer les fichiers .py de ma branche par ceux du main, je pense

--- conquest_squares_game/models.py
+++ conquest_squares_game/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
 #create database connection object
+
 db = SQLAlchemy()
 
 
@@ -43,7 +44,21 @@ class Game(db.Model):
                 " " + ("x" * table_size + " ") * (table_size - 2), 
                 "x" * (table_size - 1) + "2"
         ])
+
+    
     def apply_movement(self, player_new_x, player_new_y, new_boxes):
+        '''
+        La fonction apply_movement prend trois paramètres :
+
+        player_new_x et player_new_y : les nouvelles coordonnées du joueur qui effectue le mouvement.
+        new_boxes : la nouvelle configuration de la grille après le mouvement.
+        La fonction vérifie quel joueur est le joueur actuel (current_player) et met à jour les coordonnées du joueur correspondant .
+        Ensuite, elle passe le tour à l’autre joueur. La grille (boxes) est également mise à jour, et les changements 
+        sont sauvegardés en base de données.
+
+        La fonction retourne la position actuelle des deux joueurs, en mettant la position du joueur actuel en premier.
+        '''
+        
         if(self.player1_id == self.current_player):
             self.playerpos1_x = player_new_x
             self.playerpos1_y = player_new_y           
@@ -52,11 +67,45 @@ class Game(db.Model):
             self.playerpos2_x = player_new_x
             self.playerpos2_y = player_new_y
             self.current_player = self.player1_id
-        print(self.boxes,"-----BD----->",new_boxes)
+    
         self.boxes = new_boxes
         db.session.commit() 
         return (self.playerpos1_x, self.playerpos1_y, self.playerpos2_x, self.playerpos2_y) if self.current_player == self.player1_id else (self.playerpos2_x, self.playerpos2_y, self.playerpos1_x, self.playerpos1_y)
 
+class Q_table_test(db.Model) :
+    ___tablename__ = "q_table_test"
+
+    ''' 
+        je ne sais pas comment faire, il faut faire en sorte de lier la classe game et cette classe
+        - nécessaire ? 
+        il suffit de c'est attribut et c'est bon, peut être mettre ce changement dans 
+        la fonction apply_movement
+    '''
+
+    current_player_x = db.Column(db.Integer, db.ForeignKey('game.current_player'), primaryKey=True, nullable=False)
+    current_player_y = db.Column(db.Integer, nullable=False)
+
+    other_player_x = db.Column(db.Integer, nullable=False)
+    other_player_y = db.Column(db.Integer, nullable=False)
+
+
+
+    expectation_right = db.Column(db.Integer, nullable=True)
+    expectation_left = db.Column(db.Integer, nullable=True)
+    expectation_up = db.Column(db.Integer, nullable=True)
+    expectation_down = db.Column(db.Integer, nullable=True)
+
+    def __init__ (self,id_game, state_board, current_player_x, current_player_y, other_player_x, other_player_y, expectation_right, expectation_left, expectation_up, expectation_down):
+        self.id_game = id_game
+        self.state_board = state_board
+        self.current_player_x = current_player_x
+        self.current_player_y = current_player_y
+        self.other_player_x = other_player_x
+        self.other_player_y = other_player_y
+        self.expectation_right = expectation_right
+        self.expectation_left = expectation_left
+        self.expectation_up = expectation_up
+        self.expectation_down = expectation_down 
 
 def init_db():
     db.drop_all()
--- conquest_squares_game/views.py
+++ conquest_squares_game/views.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, jsonify 
+import time
+from collections import deque
+
+from flask import Flask, render_template, request, jsonify
 from .models import db, Player, Game
 from .ai import get_move
-import requests # todo : peut être inutile 
+from sqlalchemy.exc import SQLAlchemyError
+from .viewsFunctions import *
+
 app = Flask(__name__)
 
 app.config.from_object('config')
@@ -11,9 +16,18 @@ def index():
     return render_template("index.html")
 def content(content_id):
     return content_id
+
+'''
+    La fonction "jeu" est responsable de la gestion de la page du jeu, incluant la création des joueurs,
+    la création d'une nouvelle instance de jeu et l'envoi des informations pertinentes à la vue.
+
+    Paramètres :
+    - Aucun paramètre d'entrée pour cette fonction (utilisation d'un routeur Flask).
+
+    Résultat :
+    - Rendu de la vue "game.html" avec les informations du jeu, notamment l'ID du jeu et l'état initial de la grille (grid_state).
+    '''
 @app.route('/game')
-  # Supposons que vous ayez déjà des joueurs dans la base de données.
-    # Vous pouvez récupérer les `id_player` des joueurs ou en créer de nouveaux.
 def jeu():
     player1 = Player.query.filter_by(name="Player 1").first()
     player2 = Player.query.filter_by(name="Player IA").first()
@@ -33,104 +47,41 @@ def jeu():
     table_size = 5  # Par exemple, la taille de la table (peut être passée en paramètre)
     new_game = Game(player1_id=player1.id_player, player2_id=player2.id_player, table_size=table_size)
     db.session.add(new_game)
-    db.session.commit()  # Sauvegarde le jeu pour obtenir `id_game`
-    #print("id game : ", new_game.id_game)
-    #print("id player 1: ", player1.id_player)
-    #print("id player 2: ", player2.id_player)
+    db.session.commit()
 
     # Passer `id_game` à la vue pour l'utiliser dans le frontend
     return render_template('game.html', game_id=new_game.id_game, grid_state = new_game.boxes )
 
-# give -1 if the movement is not ok else give the new position of the player and an up to date array_string
-def is_valid_movement(movement, array_string, player):
-    # Convertit array_string en une grille de caractères
-    print("is valid début : array :", array_string)
-    lines = array_string.strip().split(' ')
-    grid = [list(line) for line in lines]
-    n = len(grid)  # Dimension de la grille
-    # Calcule la nouvelle position
-    
-    new_x = player["x"] + movement["x"]
-    new_y = player["y"] + movement["y"]
-    # Vérifie les limites de la grille et la présence d'un obstacle
-    if not (0 <= new_x < n and 0 <= new_y < n):
-        return -1  # Mouvement non autorisé
-    
-    if grid[new_y][new_x] == 'x' or int(grid[new_y][new_x]) == int( player["symbol"]):
-        
-        # Mouvement autorisé, mise à jour de la grille
-        grid[new_y][new_x] = player["symbol"]  # La nouvelle position prend le symbole du joueur
-        
-        # Reconstruit array_string avec la grille mise à jour
-        modified_array_string = ' '.join(''.join(row) for row in grid)
-        # Retourne la nouvelle position et la grille mise à jour
-        return new_x, new_y, modified_array_string
-    else :
-        return -1
-#version pour jouer avec soit même,  retirer le _NULL du chemin pour le rendre valide, et adapté les autres chemins en conséquence 
-@app.route('/travel_request_NULL', methods=['POST'])
-def travel_request_NULL():
-    data = request.json # récupére les données du client 
-    movement_x = data.get("current_x")
-    movement_y = data.get("current_y")
+'''
+    La fonction "travel_request" est responsable de traiter une requête de déplacement envoyée par le joueur humain ou l'IA,
+    et d'effectuer le mouvement approprié en mettant à jour l'état du jeu, en validant le mouvement, et en passant à l'IA si nécessaire.
 
-    game_id = data.get("game_id")
-    current_game = Game.query.get(game_id)
+    Paramètres :
+    - Aucune donnée directe, mais la requête POST doit contenir les données suivantes dans le format JSON :
+        - current_x : le déplacement horizontal demandé par le joueur humain
+        - current_y : le déplacement vertical demandé par le joueur humain
+        - game_id : l'ID du jeu actuel pour récupérer l'état du jeu depuis la base de données.
 
-    player_symbol = "1" if current_game.current_player == current_game.player1_id else "2"
-    player_x = current_game.playerpos1_x if player_symbol == "1" else current_game.playerpos2_x
-    player_y = current_game.playerpos1_y if player_symbol == "1" else current_game.playerpos2_y
-    array_string = current_game.boxes
+    Résultat :
+    - Retourne un JSON contenant :
+        - new_grid : la grille mise à jour après les mouvements du joueur humain et de l'IA
+        - new_current_player_x, new_current_player_y : les nouvelles positions du joueur courant
+        - other_x, other_y : les anciennes positions du joueur précédent
+        - winner : le gagnant actuel ou une valeur indiquant l'état de la partie.
+    '''
 
-    result = is_valid_movement({"x": movement_x, "y": movement_y},array_string,{"x": player_x, "y":player_y, "symbol": player_symbol})
-    
-    if result == - 1 :
-        return "-1"
-
-    new_x, new_y, array_string = result
-    new_player_x, new_player_y, last_player_y, last_player_x = current_game.apply_movement(new_x,new_y,array_string)
-
-    movement = get_move(game_id)
-
-    return jsonify({"new_grid" : array_string, "new_current_player_x" : new_player_x, "new_current_player_y" : new_player_y, "other_x": last_player_x, "other_y": last_player_y })
-
-def check_winner(grid_string):
-    """
-    Vérifie si la partie est terminée et détermine le gagnant.
-    
-    Paramètres:
-        grid_string (str): Une chaîne représentant l'état de la grille.
-        
-    Retourne:
-        int: 1 si le joueur humain a gagné, 2 si l'IA a gagné, 0 s'il n'y a pas encore de gagnant.
-    """
-    # Vérifie s'il reste des "x" dans la grille
-    if "x" in grid_string:
-        return 0  # Le jeu n'est pas encore terminé
-    
-    # Compte les occurrences de "1" (joueur humain) et "2" (IA) dans la grille
-    count_player1 = grid_string.count("1")
-    count_player2 = grid_string.count("2")
-    
-    # Détermine le gagnant en fonction des compteurs
-    if count_player1 > count_player2:
-        return 1  # Le joueur humain a gagné
-    elif count_player2 > count_player1:
-        return 2  # L'IA a gagné
-    else:
-        return 3  # Égalité ou aucun gagnant déterminé (optionnel)
-
-#version pour jouer contre une IA aléatoire 
 @app.route('/travel_request', methods=['POST'])
 def travel_request():
     # partie pour prendre en compte le mouvement du joueur humain
     data = request.json # récupére les données du client 
-    print("  ------------  travel request : partie humain") 
     movement_x = data.get("current_x")
     movement_y = data.get("current_y")
 
     game_id = data.get("game_id")
     current_game = Game.query.get(game_id)
+    player1_id = current_game.player1_id
+    player2_id = current_game.player2_id
+    current_player= current_game.current_player
 
     player_symbol = "1" 
     player_x = current_game.playerpos1_x
@@ -138,38 +89,34 @@ def travel_request():
     array_string = current_game.boxes
     # résultat du tableau par rapport au mouvement de l'humain
     result_human = is_valid_movement({"x": movement_x, "y": movement_y},array_string,{"x": player_x, "y":player_y, "symbol": player_symbol})
-    
-    
 
     if result_human == - 1 :
         return "-1"
-
-    new_x_human, new_y_human, array_string_human = result_human
-    # pour enregister les nouvelles positions, et position, et passé la main à l'IA 
-    last_player_x,last_player_y, new_player_x, new_player_y = current_game.apply_movement(new_x_human,new_y_human,array_string_human)
-    db.session.commit() 
-    
-    # partie pour prendre en compte le mouvement de l'IA 
-
-    print (" ------------  travel request : partie IA ")
+    if (current_player == player1_id):
+        new_x_human, new_y_human, array_string_human = result_human
+        # pour enregister les nouvelles positions, et position, et passé la main à l'IA
+        last_player_x,last_player_y, new_player_x, new_player_y = current_game.apply_movement(new_x_human,new_y_human,array_string_human)
+        current_player = player2_id
+        db.session.commit()
 
     result_IA = -1 
-    while result_IA == -1 :
-        movement = get_move() 
+    while result_IA == -1 and current_player == player2_id:
+        movement = get_move()
         
         player_symbol_IA = "2"
         player_x_IA = current_game.playerpos2_x
         player_y_IA = current_game.playerpos2_y
 
         result_IA = is_valid_movement({"x": movement["x"], "y": movement["y"]},array_string_human, {"x": player_x_IA, "y":player_y_IA, "symbol": player_symbol_IA})
-  
+    current_player = player1_id
     db.session.commit() 
-    # partie pour renvoyer le résultat au joueur sous la forme d'une modification html/JS
 
     new_x_IA, new_y_IA, array_string_IA = result_IA
     new_player_x, new_player_y, last_player_x,last_player_y = current_game.apply_movement(new_x_IA,new_y_IA,array_string_IA)
-
+    checkBoard()
+    array_string_IA = current_game.boxes
     winner = check_winner(array_string_IA)
-
-    print( "H-x :", new_player_x, "H-y ", new_player_y,"IA-x ", last_player_x,"IA-y ",last_player_y )
     return jsonify({"new_grid" : array_string_IA, "new_current_player_x" : new_player_x, "new_current_player_y" : new_player_y, "other_x": last_player_x, "other_y": last_player_y, "winner" : winner })
+
+
+
